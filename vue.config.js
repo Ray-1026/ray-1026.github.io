4483cfb14c2539c3dde6318cb0e9eb0e0df5c160
R-a-y-s <0410330@gmail.com>
2023-08-31 20:02:28 +0800

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ray-1026.github.io/'
-    : '/',
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'Ray-1026',
-      mode: 'hash'
-    }
-  },
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/ray-1026.github.io/'
+  //   : '/',
+  // pages: {
+  //   index: {
+  //     entry: 'src/main.js',
+  //     template: 'public/index.html',
+  //     filename: 'index.html',
+  //     title: 'Ray-1026',
+  //     mode: 'hash'
+  //   }
+  // },
   pluginOptions: {
     vuetify: {}
   }
