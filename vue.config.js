cceb590113cbe82e70f2cfd7f6750cae0869ad10
R-a-y-s <0410330@gmail.com>
2023-08-31 20:28:15 +0800

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,15 +5,16 @@ module.exports = defineConfig({
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/ray-1026.github.io/'
   //   : '/',
-  // pages: {
-  //   index: {
-  //     entry: 'src/main.js',
-  //     template: 'public/index.html',
-  //     filename: 'index.html',
-  //     title: 'Ray-1026',
-  //     mode: 'hash'
-  //   }
-  // },
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Ray-1026',
+      mode: 'hash'
+    },
+    subpage: 'src/main.js'
+  },
   pluginOptions: {
     vuetify: {}
   }
