87b18a9b1f37e3f6f2069366c555adc793269467
R-a-y-s <0410330@gmail.com>
2024-03-02 02:08:11 +0800

    update in 3/2

--- src/data.js
+++ src/data.js
@@ -2,8 +2,9 @@ export const articles = [
     { id: 1, title: 'First Post', time: 'Aug 16, 2023', content: '1.md' },
 ]
 export const myprojects = [
-    { title: 'My Personal Website', time: 'Aug, 2023', language: 'Vue.js, Vuetify.js', text: '', link: 'https://github.com/Ray-1026/ray-1026.github.io' },
-    { title: 'Real or AI image', time: 'Jun, 2023', language: 'Python, Tensorflow', text: 'AI final project. The goal of this project is to identify whether the images are synthesized by AI, and to test different methods to find the best performance in detecting real or AI images.', link: 'https://github.com/Ray-1026/Real-or-AI-Image' },
+    { title: 'My Personal Website', time: 'Aug, 2023', language: 'Vue.js, Vuetify.js', text: 'I am using Vue.js for a side project to create my personal website.', link: 'https://github.com/Ray-1026/ray-1026.github.io' },
+    { title: 'Reversi', time: 'Jul, 2023', language: 'Python, Pygame', text: 'The game is created for the CS camp courses.  We used Pygame as the game\'s GUI and implemented AI to playagainst the player using the Mini-max algorithm.', link: 'https://github.com/Ray-1026/Reversi' },
+    { title: 'Real or AI image', time: 'Jun, 2023', language: 'Python, Tensorflow', text: 'This is my AI final project to identify the most effective networks in detecting real or AI images.', link: 'https://github.com/Ray-1026/Real-or-AI-Image' },
+    { title: 'NYCU++', time: 'Jan, 2023', language: 'Python, Django, PostgreSQL', text: 'A basic website designed for students to upload and search for course recommendations.', link: 'https://github.com/Ray-1026/DB_Project' },
     { title: 'FPS Game', time: 'Jan, 2023', language: 'Unity', text: 'The shooting game is my final project of introduction to computer graphics.', link: 'https://github.com/Ray-1026/FPS-Game' },
-    { title: 'NYCU++', time: 'Jan, 2023', language: 'Python, Django, PostgreSQL', text: 'Final project of introduction to database system. A simple website for students in NYCU to upload and search the recommendation of courses', link: 'https://github.com/Ray-1026/DB_Project' },
 ]
